245e16e5d11a3e485b13fe446bdd03037de8279c
Deepanshu <Deepanshu.Lulla@mailchimp.com>
2023-05-04 15:57:49 -0400

    My sprint project module

--- code/bootdemo/src/main/java/com/spingdemo/bootdemo/BootdemoApplication.java
+++ code/bootdemo/src/main/java/com/spingdemo/bootdemo/BootdemoApplication.java
@@ -1,7 +1,9 @@
 package com.spingdemo.bootdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BootdemoApplication {
@@ -10,4 +12,13 @@ public class BootdemoApplication {
 		SpringApplication.run(BootdemoApplication.class, args);
 	}
 
+}
+
+
+@Component
+class StartUpRunner implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception  {
+		System.out.println("Starting up...");
+	}
 }
